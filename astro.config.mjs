eb93d9a966f2c5c4268a915b5202a307d8f8c65b
Umar Mohammad <60586376+avidcoder123@users.noreply.github.com>
2022-12-14 19:29:40 -0600

    Enable SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  output: 'server',
 });
