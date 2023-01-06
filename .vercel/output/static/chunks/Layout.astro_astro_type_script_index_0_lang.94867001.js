bc231bcb6530e076949d1c8717d6f10409fda5bc
avidcoder123 <umar.tli.edu@gmail.com>
2023-01-06 14:17:09 +0000

    Fix theme

---
+++ .vercel/output/static/chunks/Layout.astro_astro_type_script_index_0_lang.94867001.js
@@ -0,0 +1,1 @@
+import{s}from"./backend.3a7ea2ee.js";window.feedback=()=>{let e=prompt("Please enter your feedback or suggestions:");s.from("Feedback").insert({text:e}).then(()=>null)};
---
+++ .vercel/output/static/hoisted.2d452739.js
@@ -0,0 +1,1 @@
+import{s as a}from"./chunks/backend.3a7ea2ee.js";import"./chunks/_commonjsHelpers.eeb557de.js";await a.auth.getUser();location.replace("/dashboard");
