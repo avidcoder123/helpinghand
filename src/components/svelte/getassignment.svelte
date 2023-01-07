02ea7a3c0da4d9e3ee389db36d720b3e1e206d44
Umar Mohammad <umar.tli.edu@gmail.com>
2023-01-07 12:01:38 -0600

    Better text width

--- src/components/svelte/getassignment.svelte
+++ src/components/svelte/getassignment.svelte
@@ -25,8 +25,8 @@
 {#await getQuestions() then questions}
     {#each questions as question}
         <div class="flex flex-col items-center">
-            <div class="flex flex-col items-center gap-y-2 my-5">
-                <h2 class="text-white text-2xl 2xl:text-4xl text-left w-96">{question.number}. {question.text}</h2>
+            <div class="flex flex-col items-center gap-y-5 my-5">
+                <h2 class="text-white text-2xl 2xl:text-3xl text-left w-[50%]">{question.number}. {question.text}</h2>
                 {#if question.is_file}
                     <img width="30%" alt="Answers" src={"https://tzkfqozchfdkixqkvmoo.supabase.co/storage/v1/object/public/answers/" + question.answer}>
                 {:else}
