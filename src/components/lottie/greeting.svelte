b094ff6cdcd402f8c20e36e560524728b283ba2e
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-01-08 11:24:58 -0500

    why have checks failed?

---
+++ src/components/lottie/greeting.svelte
@@ -0,0 +1,32 @@
+
+<script>
+    import { LottiePlayer } from '@lottiefiles/svelte-lottie-player';
+  
+    let controlsLayout = [
+      'previousFrame',
+      'playpause',
+      'stop',
+      'nextFrame',
+      'progress',
+      'frame',
+      'loop',
+      'spacer',
+      'background',
+      'snapshot',
+      'zoom',
+      'info',
+    ];
+  </script>
+  <div class="flex justify-center flex-col w-screen items-center">
+  <LottiePlayer
+    src="export let Title:string"
+    autoplay="{true}"
+    loop="{true}"
+    controls="{false}"
+    renderer="svg"
+    background="transparent"
+    height="{450}"
+    width="{450}"
+    controlsLayout="{controlsLayout}"
+  />
+</div>
