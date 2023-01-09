726733e2ec88f89a6d99cdefbf1dfd2501285e02
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-01-09 11:58:13 -0500

    added icon and Update Log

---
+++ src/components/svelte/updateCard.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+
+export let Title: string
+
+</script>
+
+<main class="flex justify-center mt-5">
+
+<div class=" card bg-info-content sm:w-7/12 w-96">
+    <p class="text-xl pt-[5%] sm:pt-5 text-center text-white">{Title}</p>
+    <div class=" text-lg pl-2 card-body">
+        <p class="ml-5">Add new icons</p>
+        <p class="ml-5">Reverted the forest theme</p>
+        <p class="ml-5">Added update log</p>
+        <p class="ml-5">QOL changes</p>
+        <p class="ml-5">Better Layouts for various screens. Expect Changes</p>
+        <p class="ml-5">Padding and Margin issues</p>
+        <p class="ml-5">Added Dev Testing class</p>
+        <p class="ml-5">Added Animations</p>
+        <p class="ml-5">More classes</p>
+        <!-- <p class="ml-5">hi</p> -->
+    </div>
+</div>
+
+</main>
