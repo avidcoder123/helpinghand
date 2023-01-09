f0e89921c4d916716716733bef569c0b5b91c679
avidcoder123 <umar.tli.edu@gmail.com>
2023-01-09 14:12:24 +0000

    Delete vercel

--- .vercel/output/static/chunks/Layout.astro_astro_type_script_index_1_lang.2bf6f3c5.js
+++
@@ -1,1 +0,0 @@
-import{s as o}from"./backend.3a7ea2ee.js";window.LogRocket&&window.LogRocket.init("helpinghand/helpinghand");window.feedback=()=>{let e=prompt("Please enter your feedback or suggestions:");o.from("Feedback").insert({text:e}).then(()=>null)};
