b720117d174ecaad80bcc1504a481469a24f23ad
avidcoder123 <umar.tli.edu@gmail.com>
2023-01-09 14:12:10 +0000

    Fix stupid layout bugs

---
+++ .vercel/output/static/hoisted.2edc07bf.js
@@ -0,0 +1,1 @@
+import{s as a}from"./chunks/backend.3a7ea2ee.js";import"./chunks/Layout.astro_astro_type_script_index_1_lang.2bf6f3c5.js";import"./chunks/_commonjsHelpers.eeb557de.js";let r=(await a.auth.getUser()).data.user;r&&(window.location.href="/dashboard");
---
+++ .vercel/output/static/hoisted.6ceebe44.js
@@ -0,0 +1,1 @@
+import"./chunks/Layout.astro_astro_type_script_index_1_lang.2bf6f3c5.js";import"./chunks/backend.3a7ea2ee.js";import"./chunks/_commonjsHelpers.eeb557de.js";
