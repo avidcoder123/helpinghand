89c3f5531305cbecaf5896013fb8e248d9254cd6
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-02-07 19:12:22 -0500

    bug fix

--- src/components/svelte/newassignment.svelte
+++ src/components/svelte/newassignment.svelte
@@ -93,7 +93,7 @@
                     <input type="text" bind:value={question.question} placeholder="Question Text" class="input input-bordered input-primary w-96 sm:w-full sm:max-w-lg" />
                     {#if question.is_file}
                         <!-- <input bind:files={question.files} on:change={() => question.answer = Date.now().toString()} accept="image/png, image/jpeg" type="file" class="btn bg-base-content w-96 h-96 sm:w-full sm:max-w-lg" /> -->
-                        <div class="flex w-full h-screen items-center justify-center bg-grey-lighter">
+                        <div class="flex w-full pt-10 items-center justify-center bg-grey-lighter">
                             <label class="w-64 flex flex-col items-center px-4 py-6 bg-base-content rounded-lg shadow-lg tracking-wide uppercase text-white">
                                 <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
