559b5f2d4b62da117aeef8ee2d23d6fae52da743
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-03-22 17:01:33 +0000

    added search still WIP

---
+++ src/components/svelte/search.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+import { supabase } from "../../lib/backend";
+
+let List = [] as any;
+let text = '';
+
+onMount(async () => {
+  let { data: Assignment, error } = await supabase
+    .from('Assignment')
+    .select('*')
+  List = Assignment as any;
+});
+
+</script>
+
+<div>
+
+    <input type="text" bind:value={text} placeholder="Search" class=""/>
+
+</div>
