6b3d49199ab31b64527f19f7fe81ec3179a8401b
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-03-22 17:32:15 +0000

    added search still WIP

--- src/components/svelte/search.svelte
+++ src/components/svelte/search.svelte
@@ -2,15 +2,26 @@
   import { onMount } from "svelte";
 import { supabase } from "../../lib/backend";
 
-let List = [] as any;
+let results = [] as any;
 let text = '';
 
-onMount(async () => {
-  let { data: Assignment, error } = await supabase
-    .from('Assignment')
-    .select('*')
-  List = Assignment as any;
-});
+    async function searchReq(){
+    const { data: searchResults, error: er} = await supabase
+        .from('Assignments')
+        .select("*")
+        // search the title and description for the search term
+        .ilike("title", `%${text}%`)
+        .order("id", { ascending: false })
+        .limit(10)
+        console.log(searchResults)
+        // leave the results array empty if there is no search term
+        if (text.length > 0) {
+            results = searchResults || [];
+        } else {
+            results = [];
+        }
+    }
+
 
 </script>
 
