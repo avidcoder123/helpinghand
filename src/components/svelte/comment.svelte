06d63a1d7d0d8cfabfbf6ad140a7100ca21604e1
RangerDevv <meerabdul2008@gmail.com>
2023-03-25 16:39:08 -0400

    comment width fixing

--- src/components/svelte/comment.svelte
+++ src/components/svelte/comment.svelte
@@ -49,7 +49,7 @@ async function addComment() {
 
 </script>
 
-<main class="flex flex-col justify-center items-center place-items-center">
+<main class="flex flex-col justify-center items-center place-items-center w-full">
     <h1 class="text-4xl font-bold text-white text-center">Comments</h1>
     <div class="flex-row gap-6">
         <input class="input input-bordered input-info w-full max-w-xs" bind:value={comment.text} placeholder="Add a comment" type="text
